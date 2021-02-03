7a6b256c05a059c40173b2cfa81289f23029e0f0
klsklnPITX <67044656+klsklnPITX@users.noreply.github.com>
2021-02-03 10:38:25 +0100

    agent, lead model

---
+++ leads/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+
+class Agent(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+
+
+class Lead(models.Model):
+
+    # SOURCE_CHOICES = (
+    #     ("YouTube", "YouTube"),
+    #     ("Google", "Google"),
+    #     ("Newsletter", "Newsletter"),
+    # )
+
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    age = models.IntegerField(default=0)
+    agent = models.ForeignKey(Agent, on_delete=models.CASCADE)
+
+    # phoned = models.BooleanField(default=False)
+    # source = models.CharField(choices=SOURCE_CHOICES, max_length=100)
+
+    # profile_picture = models.ImageField(blank=True, null=True)
+    # special_files = models.FileField(blank=True, null=True)
