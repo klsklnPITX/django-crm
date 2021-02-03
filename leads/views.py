f0e4e8169fb76b43d01bc12f33eb2616e36832d9
klsklnPITX <67044656+klsklnPITX@users.noreply.github.com>
2021-02-03 12:52:17 +0100

    added simple templates

--- leads/views.py
+++ leads/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+from .models import Lead
+
+
+def home_page(request):
+    # return render(request, "leads/home_page.html")
+
+    leads = Lead.objects.all()
+    context = {
+        "leads": leads
+    }
+
+    return render(request, "second_page.html", context)
