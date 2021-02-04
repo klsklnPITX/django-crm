017802e034ecb438d4f2ded37709980116562cf5
klsklnPITX <67044656+klsklnPITX@users.noreply.github.com>
2021-02-04 21:59:56 +0100

    added signup

--- djcrm/urls.py
+++ djcrm/urls.py
@@ -15,17 +15,22 @@ Including another URLconf
 """
 from djcrm.settings import STATIC_URL
 from django.contrib import admin
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-from leads.views import LandingPageView
+
+from leads.views import LandingPageView, SignupView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", LandingPageView.as_view(), name="landing-page"),
     path("leads/", include("leads.urls", namespace="leads")),
+    path("login/", LoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
+    path("signup/", SignupView.as_view(), name="signup"),
 ]
 
 if settings.DEBUG:
