89630404cf093d2c2f4fa8383e168606d92a37f7
klsklnPITX <67044656+klsklnPITX@users.noreply.github.com>
2021-02-04 13:25:57 +0100

    added send mail

--- djcrm/urls.py
+++ djcrm/urls.py
@@ -13,8 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from djcrm.settings import STATIC_URL
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 from leads.views import LandingPageView
 
@@ -24,3 +27,6 @@ urlpatterns = [
     path("", LandingPageView.as_view(), name="landing-page"),
     path("leads/", include("leads.urls", namespace="leads")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
