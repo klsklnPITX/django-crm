8a4eb9e4e309866c8bf033100098e2b2b44324fd
klsklnPITX <67044656+klsklnPITX@users.noreply.github.com>
2021-02-04 22:41:21 +0100

    added signal user profile

--- leads/models.py
+++ leads/models.py
@@ -1,13 +1,23 @@
 from django.db import models
+from django.db.models.signals import post_save
 from django.contrib.auth.models import AbstractUser
+from django.db.models.deletion import CASCADE
 
 
 class User(AbstractUser):
     pass
 
 
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=CASCADE)
+
+    def __str__(self):
+        return self.user.username
+
+
 class Agent(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    organisation = models.ForeignKey(UserProfile, on_delete=CASCADE)
 
     def __str__(self):
         return self.user.email
@@ -34,3 +44,11 @@ class Lead(models.Model):
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
+
+
+def post_user_created_signal(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+
+post_save.connect(post_user_created_signal, sender=User)
